cf9d1d33b17c02de99cae8438571dbf0b700b950
guilhermem0101 <guilherme.sou@hotmail.com>
2021-12-01 18:18:52 -0300

    blog

---
+++ src/app/cadastrar/cadastrar.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.css']
+})
+export class CadastrarComponent implements OnInit {
+
+  usuario: Usuario = new Usuario
+  confirmSenha: string
+  tipoUsuario:string
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit()  {
+    window.scroll(0,0)
+    
+  }
+
+  confirmeSenha(event: any){
+    this.confirmSenha = event.target.value
+
+  }
+  tipoUser(event: any){
+    this.tipoUsuario=event.target.value
+  }
+
+  cadastrar(){
+    this.usuario.tipo = this.tipoUsuario
+
+    if(this.usuario.senha != this.confirmSenha){
+      alert('senhas diferentes')
+    }
+    else{
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario =resp
+        this.router.navigate(['/entrar'])
+        alert('Usuario cadastrado')
+      })
+    }
+  }
+}
